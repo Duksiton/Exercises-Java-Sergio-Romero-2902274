5b1add7ccf4c25f3d73557f1ff67981ff64cc95c
Duksoon <sergioromerogus7@gmail.com>
2024-02-22 18:19:23 -0500

    La mitad de los operadores ya han sido realizados; el menú y sus opciones están funcionando correctamente.

--- app/src/main/java/co/edu/sena/opp_2902274/App.java
+++ app/src/main/java/co/edu/sena/opp_2902274/App.java
@@ -54,19 +54,56 @@ public class App
     }
 
     static void area() {
+        System.out.println("Area de un triangulo");
 
+        System.out.println("Ingrese la base");
+        Scanner src = new Scanner(System.in);
+
+        double base = src.nextDouble();
+        System.out.println("Ingrese la altura");
+
+        double height = src.nextDouble();
+        double result = (base * height) / 2;
+
+        System.out.println("El area del triagunlo es: " + result);
     }
 
     static void sum() {
+        System.out.println("Suma de números");
 
+        System.out.println("Ingrese un primer número: ");
+        Scanner src = new Scanner(System.in);
+        int number1 = src.nextInt();
+
+        System.out.println("Ingrese un segundo número: ");
+        int number2 = src.nextInt();
+
+        int result = number1 + number2;
+        System.out.println("El resultado de la operación es: " + result);
     }
 
     static void raise() {
+        System.out.println("Número elevado al cuadrado");
 
+        System.out.println("Ingrese el número que desea elevar: ");
+        Scanner src = new Scanner(System.in);
+
+        int number = src.nextInt();
+
+        int result = number * number;
+        System.out.println("El número elevado es: " + result);
     }
 
     static void converter() {
+        System.out.println("Convertidor de euros a dólares");
 
+        System.out.println("Ingrese el valor que desea convertir: ");
+        Scanner src = new Scanner(System.in);
+        int value = src.nextInt();
+
+        double result = value* 1.08;
+
+        System.out.println("Su valor de " + value + " euros " + "equivale a: " + result + " dólares");
     }
 
     static void square() {
@@ -80,6 +117,7 @@ public class App
     static void radio() {
 
     }
+
     static void average() {
 
     }
