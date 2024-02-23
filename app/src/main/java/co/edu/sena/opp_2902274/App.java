a96c9a2331a91316f9f23772e60bea105b794af4
Duksoon <sergioromerogus7@gmail.com>
2024-02-22 19:52:12 -0500

    Los 8 ejercicios se han terminado con exito, el menu y cada ejercicio funciona de manera satisfactoria

--- app/src/main/java/co/edu/sena/opp_2902274/App.java
+++ app/src/main/java/co/edu/sena/opp_2902274/App.java
@@ -54,7 +54,7 @@ public class App
     }
 
     static void area() {
-        System.out.println("Area de un triangulo");
+        System.out.println("Area de un triángulo");
 
         System.out.println("Ingrese la base");
         Scanner src = new Scanner(System.in);
@@ -65,7 +65,7 @@ public class App
         double height = src.nextDouble();
         double result = (base * height) / 2;
 
-        System.out.println("El area del triagunlo es: " + result);
+        System.out.println("El area del triángulo es: " + result);
     }
 
     static void sum() {
@@ -107,19 +107,64 @@ public class App
     }
 
     static void square() {
+        System.out.println("Lado de un cuadrado que muestre el área y el perímetro");
+        System.out.println("Ingrese el lado del cuadrado: ");
+        Scanner src = new Scanner(System.in);
+        int side = src.nextInt();
 
+        int result = side*side;
+        int result2 = 4*side;
+
+        System.out.println("El lado de su cuadrado es de: " + side +"," + " el área es de: " + result +","  + " y el perímetro de: " + result2);
     }
 
     static void cylinder() {
+        System.out.println("Área y volumen de un cilindro");
+       
+        System.out.println("Ingrese el radio del cilindro: ");
+        Scanner src = new Scanner(System.in);
+        int radio = src.nextInt();
+
+        System.out.println("Ingrese la altura del cilindro: ");
+        int height = src.nextInt();
+  
+        
+        double total_area= 2 * Math.PI * radio * (radio + height);
+        double volume = Math.PI * Math.pow(radio, 2) * height;
+
+        System.out.println("El área total es de: " + total_area + " y el volumen es de: " + volume);
 
     }
 
     static void radio() {
+        System.out.println("Radio de una circunferencia");
+       
+        System.out.println("Ingrese el radio de la circunferencia: ");
+        Scanner src = new Scanner(System.in);
+        int radio = src.nextInt();
 
+        double length = 2 * Math.PI * radio;
+        double area= Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El radio de la circunferencia es de: " + radio + " la longitud es de " + length + " y el área es de " + area);
     }
 
     static void average() {
+        System.out.println("Promedio de 3 números");
 
+        System.out.println("Ingrese el primer número: ");
+        Scanner src = new Scanner(System.in);
+        int num1 = src.nextInt();
+
+        System.out.println("Ingrese el segundo número: ");
+        int num2 = src.nextInt();
+        
+        System.out.println("Ingrese el tercer número: ");
+        int num3 = src.nextInt();
+
+        int result = (num1 + num2 + num3)/3;
+
+        System.out.println("Sus 3 números fueron: " + num1 +", " + num2 +", " + num3 + " y el promedio es de: " + result);
     }
 
 }
